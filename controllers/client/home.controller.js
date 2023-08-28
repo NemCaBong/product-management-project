5eee2696f36bf7deff7873973fde3da8a6d10b97
NemCaBong <89348778+NemCaBong@users.noreply.github.com>
2023-08-28 21:24:16 +0700

    Init project

---
+++ controllers/client/home.controller.js
@@ -0,0 +1,4 @@
+// index la controller  danh cho trang chu
+module.exports.index = (req, res) => {
+	res.render("client/pages/home/index.pug");
+};
---
+++ controllers/client/product.controller.js
@@ -0,0 +1,5 @@
+// day la index cua trang products
+// phan biet voi cac trang khac
+module.exports.index = (req, res) => {
+	res.render("client/pages/products/index.pug");
+};
