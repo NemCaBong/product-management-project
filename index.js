9123b01c65024044d890b96d3536859445585bcf
NemCaBong <89348778+NemCaBong@users.noreply.github.com>
2023-08-31 10:06:42 +0700

    Build general layout for admin page

--- index.js
+++ index.js
@@ -1,21 +1,28 @@
 const express = require("express");
+const app = express();
+// import .env file
 require("dotenv").config();
 
+// connect mongodb
 const database = require("./config/database");
 database.connect();
-
+// config port
 const port = process.env.PORT;
 
-const app = express();
+// App Local Var
+const systemConfig = require("./config/system");
+app.locals.prefixAdmin = systemConfig.prefixAdmin;
+
 app.use(express.static("public"));
 app.set("views", "./views");
 app.set("view engine", "pug");
 
 // Routes
-const route = require("./routes/client/index.route");
 const routeAdmin = require("./routes/admin/index.route");
-route(app);
+const route = require("./routes/client/index.route");
 routeAdmin(app);
+route(app);
+
 app.listen(port, () => {
 	console.log(`Example app listening on port: ${port}`);
 });
