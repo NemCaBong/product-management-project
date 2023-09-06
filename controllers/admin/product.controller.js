bfaddce45021048d90c28b824041acc3de93122a
NemCaBong <89348778+NemCaBong@users.noreply.github.com>
2023-09-06 21:39:37 +0700

    Finish searching features

--- controllers/admin/product.controller.js
+++ controllers/admin/product.controller.js
@@ -2,45 +2,54 @@ const Product = require("../../models/product.model");
 
 // [GET] /admin/products
 module.exports.index = async (req, res) => {
-	let find = {
-		deleted: false,
-	};
+  // thuoc tinh class de truyen classCss vao
+  let filterStatus = [
+    {
+      name: "Tất cả",
+      status: "",
+      class: "",
+    },
+    {
+      name: "Hoạt động",
+      status: "active",
+      class: "",
+    },
+    {
+      name: "Dừng hoạt động",
+      status: "inactive",
+      class: "",
+    },
+  ];
+  let find = {
+    deleted: false,
+  };
 
-	// thuoc tinh class de truyen classCss vao
-	let filterStatus = [
-		{
-			name: "Tất cả",
-			status: "",
-			class: "",
-		},
-		{
-			name: "Hoạt động",
-			status: "active",
-			class: "",
-		},
-		{
-			name: "Dừng hoạt động",
-			status: "inactive",
-			class: "",
-		},
-	];
+  if (req.query.status) {
+    const index = filterStatus.findIndex(
+      (item) => item.status === req.query.status
+    );
+    filterStatus[index].class = "active";
+  } else {
+    filterStatus[0].class = "active";
+  }
+  if (req.query.status) {
+    // neu co query status => cho vao find.
+    find.status = req.query.status;
+  }
 
-	if (req.query.status) {
-		const index = filterStatus.findIndex(
-			(item) => item.status === req.query.status
-		);
-		filterStatus[index].class = "active";
-		// neu co query status => cho vao find.
-		find.status = req.query.status;
-	} else {
-		filterStatus[0].class = "active";
-	}
+  let keyword = "";
+  if (req.query.keyword) {
+    keyword = req.query.keyword;
+    const regex = new RegExp(keyword, "i");
+    find.title = regex;
+  }
 
-	const products = await Product.find(find);
+  const products = await Product.find(find);
 
-	res.render("admin/pages/products/index", {
-		pageTitle: "Danh sách sản phẩm",
-		products: products,
-		filterStatus: filterStatus,
-	});
+  res.render("admin/pages/products/index", {
+    pageTitle: "Danh sách sản phẩm",
+    products: products,
+    filterStatus: filterStatus,
+    keyword: keyword,
+  });
 };
