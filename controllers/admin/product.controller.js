ddd91634b9890893556dad779733b719f8d26b71
NemCaBong <89348778+NemCaBong@users.noreply.github.com>
2023-09-07 12:14:35 +0700

    Finish pagination features

--- controllers/admin/product.controller.js
+++ controllers/admin/product.controller.js
@@ -1,6 +1,8 @@
 const Product = require("../../models/product.model");
 
 const filterStatusHelper = require("../../helpers/filterStatus");
+
+const searchHelper = require("../../helpers/search");
 // [GET] /admin/products
 module.exports.index = async (req, res) => {
   // bộ lọc
@@ -16,20 +18,42 @@ module.exports.index = async (req, res) => {
     find.status = req.query.status;
   }
 
-  // phan tim kiem.
-  let keyword = "";
-  if (req.query.keyword) {
-    keyword = req.query.keyword;
-    const regex = new RegExp(keyword, "i");
-    find.title = regex;
+  // phan tim kiem
+  const objectSearch = searchHelper(req.query);
+  if (objectSearch.keyword) {
+    find.title = objectSearch.keywordRegex;
+  }
+  // het tim kiem
+
+  // phan phan trang
+  const objectPagination = {
+    currentPage: 1,
+    limitItems: 5,
+  };
+  if (req.query.page) {
+    objectPagination.currentPage = parseInt(req.query.page);
   }
 
-  const products = await Product.find(find);
+  // tồng sản phẩm trong DB.
+  const totalItems = await Product.count(find);
+  // tổng số trang cần
+  objectPagination.totalPages = Math.ceil(
+    totalItems / objectPagination.limitItems
+  );
+
+  // sản phẩm bắt đầu lấy
+  objectPagination.skip =
+    (objectPagination.currentPage - 1) * objectPagination.limitItems;
+
+  const products = await Product.find(find)
+    .skip(objectPagination.skip)
+    .limit(objectPagination.limitItems);
 
   res.render("admin/pages/products/index", {
     pageTitle: "Danh sách sản phẩm",
     products: products,
     filterStatus: filterStatus,
-    keyword: keyword,
+    keyword: objectSearch.keyword,
+    pagination: objectPagination,
   });
 };
--- public/admin/js/script.js
+++ public/admin/js/script.js
@@ -1,38 +1,52 @@
 const buttonStatus = document.querySelectorAll("[button-status]");
 
 if (buttonStatus.length > 0) {
-	// create new URL obj using current URL location
-	let url = new URL(window.location.href);
-	buttonStatus.forEach((button) => {
-		button.addEventListener("click", () => {
-			const status = button.getAttribute("button-status");
+  // create new URL obj using current URL location
+  let url = new URL(window.location.href);
+  buttonStatus.forEach((button) => {
+    button.addEventListener("click", () => {
+      const status = button.getAttribute("button-status");
 
-			if (status) {
-				url.searchParams.set("status", status);
-			} else {
-				url.searchParams.delete("status");
-			}
-			// override the current address
-			window.location.href = url;
-		});
-	});
+      if (status) {
+        url.searchParams.set("status", status);
+      } else {
+        url.searchParams.delete("status");
+      }
+      // override the current address
+      window.location.href = url;
+    });
+  });
 }
 // End Button Status
 
 // Form Search
 const formSearch = document.getElementById("form-search");
 if (formSearch) {
-	let url = new URL(window.location.href);
+  let url = new URL(window.location.href);
 
-	formSearch.addEventListener("submit", (e) => {
-		// ngan cho form chuyen website den 1 URL moi
-		e.preventDefault();
-		const keyword = e.target.elements.keyword.value;
-		if (keyword) {
-			url.searchParams.set("keyword", keyword);
-		} else {
-			url.searchParams.delete("keyword");
-		}
-		window.location.href = url;
-	});
+  formSearch.addEventListener("submit", (e) => {
+    // ngan cho form chuyen website den 1 URL moi
+    e.preventDefault();
+    const keyword = e.target.elements.keyword.value;
+    if (keyword) {
+      url.searchParams.set("keyword", keyword);
+    } else {
+      url.searchParams.delete("keyword");
+    }
+    window.location.href = url;
+  });
+}
+
+// Pagination
+const paginationButtons = document.querySelectorAll("[button-page]");
+if (paginationButtons) {
+  let url = new URL(window.location.href);
+  for (let button of paginationButtons) {
+    button.addEventListener("click", () => {
+      const pageNum = button.getAttribute("button-page");
+      console.log(pageNum);
+      url.searchParams.set("page", pageNum);
+      window.location.href = url.href;
+    });
+  }
 }
