b76321900b26447371ea0683e0f94179b3007a26
unknown <Minh Hoang>
2023-09-15 21:38:32 +0700

    Create preview image feature

--- public/admin/js/script.js
+++ public/admin/js/script.js
@@ -153,3 +153,20 @@ if (showAlert) {
   });
 }
 // End Modal
+
+// Preview image
+const uploadImage = document.querySelector("[upload-image]");
+if (uploadImage) {
+  const uploadImageInput = document.querySelector("[upload-image-input]");
+  const uploadImagePreview = document.querySelector("[upload-image-preview]");
+
+  uploadImageInput.addEventListener("change", (e) => {
+    console.log(e);
+    const file = e.target.files[0];
+    if (file) {
+      uploadImagePreview.src = URL.createObjectURL(file);
+    }
+  });
+}
+
+// End preview image.
