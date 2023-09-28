1d2661451912a090e21b2ee6f089451c845df185
unknown <Minh Hoang>
2023-09-29 00:09:28 +0700

    Finish parent-child relation in category

--- controllers/admin/product-category.controller.js
+++ controllers/admin/product-category.controller.js
@@ -1,6 +1,6 @@
 const ProductCategory = require("../../models/product-category.model");
 const systemConfig = require("../../config/system");
-
+const createTreeHelper = require("../../helpers/createTree");
 // [GET] /admin/product-category
 module.exports.index = async (req, res) => {
   let find = {
@@ -8,17 +8,26 @@ module.exports.index = async (req, res) => {
   };
 
   const productCategories = await ProductCategory.find(find);
+  const newRecords = createTreeHelper.tree(productCategories);
 
   res.render("admin/pages/product-category/index", {
     pageTitle: "Danh mục sản phẩm",
-    records: productCategories,
+    records: newRecords,
   });
 };
 
 // [GET] /admin/product-category/create
 module.exports.create = async (req, res) => {
+  let find = {
+    deleted: false,
+  };
+
+  const records = await ProductCategory.find(find);
+
+  const newRecords = createTreeHelper.tree(records);
   res.render("admin/pages/product-category/create", {
     pageTitle: "Tạo danh mục",
+    records: newRecords,
   });
 };
 
