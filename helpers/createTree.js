fbc565eba778d93cf910625e60be53c8cbfa0807
unknown <Minh Hoang>
2023-09-29 00:48:10 +0700

    Fix the STT

--- helpers/createTree.js
+++ helpers/createTree.js
@@ -1,10 +1,12 @@
+let count = 0;
 const createTree = (arr, parentId = "") => {
   const tree = [];
 
   arr.forEach((item) => {
     if (item.parent_id === parentId) {
       const newItem = item;
-
+      count++;
+      newItem.index = count;
       const children = createTree(arr, item.id);
 
       if (children.length > 0) {
@@ -22,6 +24,9 @@ const createTree = (arr, parentId = "") => {
 // cái hàm đệ quy là gì nên chúng ta cần export 1 cái hàm khác chứa hàm đệ quy đó
 
 module.exports.tree = (arr, parentId = "") => {
+  // make count to 0 after every reload
+  // because server not reload.
+  count = 0;
   const tree = createTree(arr, (parentId = ""));
 
   return tree;
