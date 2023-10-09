2562b37e870483bde83a2d0f6d1ba9c80a63233a
unknown <Minh Hoang>
2023-10-09 16:18:43 +0700

    Them authorization cho website

--- middlewares/admin/authen.middleware.js
+++ middlewares/admin/authen.middleware.js
@@ -1,6 +1,6 @@
 const systemConfig = require("../../config/system");
 const Account = require("../../models/account.model");
-
+const Role = require("../../models/role.model");
 module.exports.requireAuth = async (req, res, next) => {
   // Hàm bắt buộc phải chạy qua.
   // tất cả key trong cookies đều ở trong req.cookies
@@ -10,8 +10,16 @@ module.exports.requireAuth = async (req, res, next) => {
     const user = await Account.findOne({
       deleted: false,
       token: req.cookies.token,
-    });
+    }).select("-password");
     if (user) {
+      const role = await Role.findOne({
+        _id: user.role_id,
+      });
+
+      // tạo ra biến toàn cục là user và role
+      // để sử dụng trong toàn apps.
+      res.locals.user = user;
+      res.locals.role = role;
       next();
     } else {
       res.redirect(`${systemConfig.prefixAdmin}/auth/login`);
