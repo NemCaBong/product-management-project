efe4837a4720cb2e8d5835d1be1853c4d07e0367
unknown <Minh Hoang>
2023-11-04 10:54:25 +0700

    Gui otp qua mail

--- controllers/client/user.controller.js
+++ controllers/client/user.controller.js
@@ -1,5 +1,9 @@
 const md5 = require("md5");
 const User = require("../../models/user.model");
+const ForgotPassword = require("../../models/forgot-password.model");
+const generate = require("../../helpers/generate");
+const sendMailHelper = require("../../helpers/sendMail");
+
 // [GET] /user/register
 module.exports.register = async (req, res) => {
   res.render("client/pages/user/register", {
@@ -38,7 +42,7 @@ module.exports.registerPost = async (req, res) => {
 // [GET] /user/login
 module.exports.login = async (req, res) => {
   res.render("client/pages/user/login", {
-    pageTitle: "Đăng nhập tài kho",
+    pageTitle: "Đăng nhập tài khoản",
   });
 };
 
@@ -79,5 +83,124 @@ module.exports.loginPost = async (req, res) => {
 module.exports.logout = async (req, res) => {
   // Xóa token trong cookies là xong
   res.clearCookie("tokenUser");
+  res.redirect("/");
+};
+
+// [GET] /user/password/forgot
+module.exports.forgotPassword = async (req, res) => {
+  res.render("client/pages/user/forgot-password", {
+    pageTitle: "Quên mật khẩu",
+  });
+};
+
+// [POST] /user/password/forgot
+module.exports.forgotPasswordPost = async (req, res) => {
+  const email = req.body.email;
+
+  // tìm email có tồn tại hay ko.
+  const user = await User.findOne({
+    email: email,
+    deleted: false,
+  });
+
+  if (!user) {
+    req.flash("error", `Email ${email} không tồn tại`);
+    res.redirect("back");
+    return;
+  }
+  // Nếu muốn thêm tính năng ko gửi liên tục OTP thì
+  // có thể check bảng forgotpassword nếu có email đó rồi thì ko gửi cái mới nx
+
+  // Nếu có tk đó thì chúng ta
+  // Lưu thông tin vào DB
+  const otp = generate.generateRandomNumber(8);
+
+  const objectForgotPassword = {
+    email: email,
+    otp: otp,
+    // 10 từ thời gian hiện tại
+    expireAt: Date.now(),
+  };
+
+  const forgotPassword = new ForgotPassword(objectForgotPassword);
+  await forgotPassword.save();
+
+  // Nếu tồn tại email thì gửi mã OTP qua mail. (viết sau)
+  const subject = "Mã OTP xác minh lấy lại mk";
+  const html = `Mã OTP để lấy lại mật khẩu là: <b>${otp}</b>. Thời hạn sử dụng là 3 phút.`;
+
+  sendMailHelper.sendMail(email, subject, html);
+
+  // thêm cái query email vào để bên kia lấy email luôn.
+  res.redirect(`/user/password/otp?email=${email}`);
+};
+
+// [GET] /user/password/otp
+module.exports.otpPassword = async (req, res) => {
+  const email = req.query.email;
+  res.render("client/pages/user/otp-password", {
+    pageTitle: "Nhập mã OTP",
+    email: email,
+  });
+};
+
+// [POST] /user/password/otp
+module.exports.otpPasswordPost = async (req, res) => {
+  // bây giờ email sẽ gửi qua body của form
+  const email = req.body.email;
+  const otp = req.body.otp;
+
+  const result = await ForgotPassword.findOne({
+    email: email,
+    otp: otp,
+  });
+
+  // Nếu không hợp lệ
+  if (!result) {
+    req.flash("error", "OTP không hợp lệ");
+    res.redirect("back");
+    return;
+  }
+
+  // Nếu ông ấy có mã OTP rồi coi như cta xác thực thành công
+  // Trả lại cho ông ấy cái token user của mình, lưu vào cookies.
+  // để khi ngta thay đổi mk thì gửi ngầm theo cái token để biết là user
+  // nào để có thể chỉnh sửa
+
+  const user = await User.findOne({
+    email: email,
+  });
+  // đưa token vào cookies.
+  res.cookie("tokenUser", user.tokenUser);
+
+  // sang trang thay đổi mk
+  res.redirect("/user/password/reset");
+};
+
+// [GET] /user/password/reset
+module.exports.resetPassword = async (req, res) => {
+  const tokenUser = req.cookies.tokenUser;
+
+  res.render("client/pages/user/reset-password", {
+    pageTitle: "Thay đổi mật khẩu",
+  });
+};
+
+// [POST] /user/password/reset
+module.exports.resetPasswordPost = async (req, res) => {
+  // việc check confirmPass với password được check tại phần
+  // validates được dùng trong thư mục user.route rồi.
+  const password = req.body.password;
+  const tokenUser = req.cookies.tokenUser;
+
+  await User.updateOne(
+    {
+      tokenUser: tokenUser,
+    },
+    {
+      password: md5(password),
+    }
+  );
+  // trở về trang chủ
   res.redirect("/");
 };
--- routes/client/user.route.js
+++ routes/client/user.route.js
@@ -3,12 +3,30 @@ const controller = require("../../controllers/client/user.controller");
 const router = express.Router();
 const userValidate = require("../../validates/client/user.validate");
 
+// Phần đường dẫn register
 router.get("/register", controller.register);
 router.post("/register", userValidate.registerPost, controller.registerPost);
 
+// phần đường dẫn login
 router.get("/login", controller.login);
 router.post("/login", userValidate.loginPost, controller.loginPost);
 
 // Tính năng đăng xuất không cần gửi bằng phg thức POST.
+// bởi không cần bảo mật.
 router.get("/logout", controller.logout);
+
+// Phần đường dẫn /password
+router.get("/password/forgot", controller.forgotPassword);
+router.post("/password/forgot", controller.forgotPasswordPost);
+// otp
+router.get("/password/otp", controller.otpPassword);
+router.post("/password/otp", controller.otpPasswordPost);
+
+// reset
+router.get("/password/reset", controller.resetPassword);
+router.post(
+  "/password/reset",
+  userValidate.resetPasswordPost,
+  controller.resetPasswordPost
+);
 module.exports = router;
