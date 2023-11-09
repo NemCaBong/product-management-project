ac8b09faedaaafff2495287694c6815e8df3eb02
unknown <Minh Hoang>
2023-11-09 09:33:59 +0700

    Lam giao dien cho /chat

--- routes/client/index.route.js
+++ routes/client/index.route.js
@@ -4,6 +4,7 @@ const searchRoutes = require("./search.route");
 const cartRoutes = require("./cart.route");
 const checkoutRoutes = require("./checkout.route");
 const userRoutes = require("./user.route");
+const chatRoutes = require("./chat.route");
 // middleware
 const categoryMiddleware = require("../../middlewares/client/category.middleware");
 const cartMiddleware = require("../../middlewares/client/cart.middleware");
@@ -21,6 +22,7 @@ module.exports = (app) => {
   app.use(cartMiddleware.cartId);
 
   app.use("/", homeRoutes);
+  app.use("/chat", chatRoutes);
   app.use("/cart", cartRoutes);
   app.use("/user", userRoutes);
   app.use("/search", searchRoutes);
