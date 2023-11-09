a559f32fd5b26f9f9a8d5072b0f2488a2a180d02
unknown <Minh Hoang>
2023-11-09 11:51:21 +0700

    Lam tinh nang chat co ban

--- controllers/client/chat.controller.js
+++ controllers/client/chat.controller.js
@@ -1,11 +1,43 @@
+const Chat = require("../../models/chat.model");
+const User = require("../../models/user.model");
 // [GET] /chat
 module.exports.index = async (req, res) => {
+  // lấy ra userid theo res.locals
+  // bởi đã force phải có res.locals.user trong file auth.middleware
+  const userID = res.locals.user.id;
+
   // dùng biến _io của global
-  _io.on("connection", (socket) => {
-    console.log("a user connected", socket.id);
+  // socket.io
+  _io.once("connection", (socket) => {
+    // console.log("a user connected", socket.id);
+
+    socket.on("CLIENT_SEND_MESSAGE", async (content) => {
+      const chat = new Chat({
+        user_id: userID,
+        content: content,
+      });
+      // lưu đoạn code vào db.
+      await chat.save();
+    });
   });
+
+  // end socket.io
+
+  const chats = await Chat.find({
+    deleted: false,
+  });
+
+  for (let chat of chats) {
+    const infoUser = await User.findOne({
+      _id: chat.user_id,
+      deleted: false,
+    }).select("fullName");
+    // gán thêm infoUser cho đoạn tin nhắn
+    chat.infoUser = infoUser;
+  }
 
   res.render("client/pages/chat/index", {
     pageTitle: "Trang chat",
+    chats: chats,
   });
 };
---
+++ public/js/chat.js
@@ -0,0 +1,16 @@
+// CLIENT_SEND_MESSAGE
+const formSendData = document.querySelector(".chat .inner-form");
+if (formSendData) {
+  formSendData.addEventListener("submit", (e) => {
+    e.preventDefault();
+
+    // content là name của thẻ input.
+    const content = e.target.elements.content.value;
+
+    if (content) {
+      socket.emit("CLIENT_SEND_MESSAGE", content);
+      e.target.elements.content.value = "";
+    }
+  });
+}
+// END CLIENT_SEND_MESSAGE
